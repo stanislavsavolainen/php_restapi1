e1334f608cfabfa4cc1aa1b163fb9d8734ad13fc
unk900t2 <stanislav.savolainen@mail.com>
2020-10-07 03:53:06 +0300

    change long text line

--- progress5/getBook.php
+++ progress5/getBook.php
@@ -106,8 +106,9 @@ if (isset($_GET['isbn'])) {
 		}
 
 		if( ! $deviceAndTokenMatch ){
-			$jwtJson = array("error_msg" =>  "no valid jwt-token found. This error caused by following reason : cookie are disabled and  deviceId(cookie) is not initalized. Your actions are refresh browser page and allow cookie on this page.");
-						echo json_encode($jwtJson);
+			$jwtJson = array("error_msg" =>  "no valid jwt-token found. This error caused by following reason : 
+			cookie are disabled and  deviceId(cookie) is not initalized. Your actions are refresh browser page and allow cookie on this page.");
+			echo json_encode($jwtJson);
 		}
 
 
--- progress5/getMovie.php
+++ progress5/getMovie.php
@@ -126,8 +126,9 @@ if (isset($_GET['jwtstring'])  &&  isset($_GET['deviceId'])) {
 	} 
 
 	if( ! $deviceAndTokenMatch ){
-		$jwtJson = array("error_msg" =>  "no valid jwt-token found. This error caused by following reason : cookies are disabled and  deviceId(cookie) is not initalized. Your actions are refresh browser page and allow cookie on this page.");
-					echo json_encode($jwtJson);
+		$jwtJson = array("error_msg" =>  "no valid jwt-token found. This error caused by following reason : 
+		cookies are disabled and  deviceId(cookie) is not initalized. Your actions are refresh browser page and allow cookie on this page.");
+		echo json_encode($jwtJson);
 	}
 
 
